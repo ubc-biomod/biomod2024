7daf228a40f68e9f69d923c3ac896f71063a6ab9
JosephDeng317 <75818199+JosephDeng317@users.noreply.github.com>
2024-10-30 19:20:35 -0700

    working but needs some improvements for mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,26 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "My Docs",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_upward",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+          },
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/globals.css",
+      ],
       social: {
         github: "https://github.com/withastro/starlight",
       },
--- src/content/docs/hero.jsx
+++ src/content/docs/hero.jsx
@@ -1,7 +1,6 @@
 // Import necessary libraries
 import React, { useState } from "react";
 import ThreeJSComponent from "./threejs.jsx";
-import "../../styles/tailwind.css";
 
 
 // The Hero component
