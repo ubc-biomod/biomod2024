872711fb997ed97ada5e8f0b3efeb61d876965de
Minh Au <auduongminh2004@gmail.com>
2024-11-04 08:36:41 -0800

    modify Header to work with config files + add some placeholder docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,47 @@ import starlight from '@astrojs/starlight';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [starlight({
-        title: 'My Docs',
+        title: 'UBC BIOMOD',
         social: {
             github: 'https://github.com/withastro/starlight',
         },
         sidebar: [
             {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
+                label: 'Home',
+                link: '/',
+                badge: 'Hidden'
             },
             {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
+                label: 'Lab Notebook',
+                autogenerate: { directory: 'lab-notebook' },
+                badge: {
+                    text: 'Call to Action',
+                }
+            },
+            {
+                label: 'Ideas',
+                autogenerate: { directory: 'ideas' },
+            },
+            {
+                label: 'ELSI',
+                autogenerate: { directory: 'elsi' },
+            },
+            {
+                label: 'Sponsors',
+                link: '/sponsors',
+                badge: 'Hidden' 
             },
         ],
-		}), tailwind()],
+        components: {
+            Header: './src/components/starlight-override/Header.astro',
+        },
+        customCss: [
+            './src/tailwind.css',
+        ]
+        }), tailwind(), react()],
 });
