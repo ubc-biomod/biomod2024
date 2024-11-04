4c7970716f31362fa7f4a25089e53b2d45a42acb
Serena <159407812+aaneres@users.noreply.github.com>
2024-11-04 13:21:35 -0800

    [Main Page] Embedded Video + Navbar (#3)
    * YouTube Video component (currently embedded into index!)
    
    * Drafted navbar!
    
    * remove extraneous HeaderWrapper
    
    * modify Header to work with config files + add some placeholder docs
    
    * modify default css for navbar container
    
    * refactor + remove extraneous folders/style files
    
    * realize docs specific header is unecessary
    
    * add custom sidebar + pagination for isolated docs
    
    * make custom components compatible with all normal pages
    
    * fix Video display + hack to get around splash template
    
    ---------
    
    Co-authored-by: Minh Au <minhau@student.ubc.ca>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,49 @@ import starlight from '@astrojs/starlight';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [starlight({
-        title: 'My Docs',
+        title: 'UBC BIOMOD',
         social: {
             github: 'https://github.com/withastro/starlight',
         },
         sidebar: [
             {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
+                label: 'Home',
+                link: '/',
+                badge: 'Hidden'
             },
             {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
+                label: 'Ideas',
+                autogenerate: { directory: 'ideas' },
+            },
+            {
+                label: 'ELSI',
+                autogenerate: { directory: 'elsi' },
+            },
+            {
+                label: 'Lab Notebook',
+                autogenerate: { directory: 'lab-notebook' },
+                badge: {
+                    text: 'Call to Action',
+                }
+            },
+            {
+                label: 'Sponsors',
+                link: '/sponsors',
+                badge: 'Hidden' 
             },
         ],
-		}), tailwind()],
+        components: {
+            Header: './src/components/overrides/Header.astro',
+            Sidebar: './src/components/overrides/Sidebar.astro',
+            Pagination: './src/components/overrides/Pagination.astro',
+        },
+        customCss: [
+            './src/tailwind.css',
+        ]
+        }), tailwind(), react()],
 });
