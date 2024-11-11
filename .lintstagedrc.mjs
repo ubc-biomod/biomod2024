56e3a1f7a6cadbfb2132318e6768273a46205ca5
Minh Au <auduongminh2004@gmail.com>
2024-11-11 14:22:44 -0800

    format project

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['prettier --write'],
-  '**/*.ts?(x)': () => 'npm run type-check',
-  '*.json': ['prettier --write'],
+  "*.{js,jsx,ts,tsx}": ["prettier --write"],
+  "**/*.ts?(x)": () => "npm run type-check",
+  "*.json": ["prettier --write"],
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,55 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'UBC BIOMOD',
-        social: {
-            github: 'https://github.com/withastro/starlight',
+  integrations: [
+    starlight({
+      title: "UBC BIOMOD",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Home",
+          link: "/",
+          badge: "Hidden",
         },
-        sidebar: [
-            {
-                label: 'Home',
-                link: '/',
-                badge: 'Hidden'
-            },
-            {
-                label: 'Ideas',
-                autogenerate: { directory: 'ideas' },
-            },
-            {
-                label: 'ELSI',
-                autogenerate: { directory: 'elsi' },
-            },
-            {
-                label: 'Lab Notebook',
-                autogenerate: { directory: 'lab-notebook' },
-                badge: {
-                    text: 'Call to Action',
-                }
-            },
-            {
-                label: 'Sponsors',
-                link: '/sponsors',
-                badge: 'Hidden' 
-            },
-        ],
-        components: {
-            Header: './src/components/overrides/Header.astro',
-            Sidebar: './src/components/overrides/Sidebar.astro',
-            Pagination: './src/components/overrides/Pagination.astro',
-            Footer: './src/components/overrides/FooterWrapper.astro',
+        {
+          label: "Ideas",
+          autogenerate: { directory: "ideas" },
         },
-        customCss: [
-            './src/tailwind.css',
-        ]
-        }), tailwind(), react()],
+        {
+          label: "ELSI",
+          autogenerate: { directory: "elsi" },
+        },
+        {
+          label: "Lab Notebook",
+          autogenerate: { directory: "lab-notebook" },
+          badge: {
+            text: "Call to Action",
+          },
+        },
+        {
+          label: "Sponsors",
+          link: "/sponsors",
+          badge: "Hidden",
+        },
+      ],
+      components: {
+        Header: "./src/components/overrides/Header.astro",
+        Sidebar: "./src/components/overrides/Sidebar.astro",
+        Pagination: "./src/components/overrides/Pagination.astro",
+        Footer: "./src/components/overrides/FooterWrapper.astro",
+      },
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind(),
+    react(),
+  ],
 });
