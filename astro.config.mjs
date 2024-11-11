f0148a2373955e10714afe9c9bc11a7aa5b4c02c
Minh Au <minhau@student.ubc.ca>
2024-11-11 14:53:20 -0800

    [MISC] Add configs for deployment (#15)
    * Add configs for deployment
    
    * ignoring .netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,46 +4,47 @@ import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "UBC BIOMOD",
+  integrations: [starlight({
+      title: 'UBC BIOMOD',
       social: {
-        github: "https://github.com/withastro/starlight",
+          github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
-        {
-          label: "Home",
-          link: "/",
-          badge: "Hidden",
-        },
-        {
-          label: "Ideas",
-          autogenerate: { directory: "ideas" },
-        },
-        {
-          label: "ELSI",
-          autogenerate: { directory: "elsi" },
-        },
-        {
-          label: "Lab Notebook",
-          autogenerate: { directory: "lab-notebook" },
-          badge: {
-            text: "Call to Action",
+          {
+              label: 'Home',
+              link: '/',
+              badge: 'Hidden'
           },
-        },
-        {
-          label: "Sponsors",
-          link: "/sponsors",
-          badge: "Hidden",
-        },
+          {
+              label: 'Ideas',
+              autogenerate: { directory: 'ideas' },
+          },
+          {
+              label: 'ELSI',
+              autogenerate: { directory: 'elsi' },
+          },
+          {
+              label: 'Lab Notebook',
+              autogenerate: { directory: 'lab-notebook' },
+              badge: {
+                  text: 'Call to Action',
+              }
+          },
+          {
+              label: 'Sponsors',
+              link: '/sponsors',
+              badge: 'Hidden' 
+          },
       ],
       components: {
-        Header: "./src/components/overrides/Header.astro",
-        Sidebar: "./src/components/overrides/Sidebar.astro",
-        Pagination: "./src/components/overrides/Pagination.astro",
-        Footer: "./src/components/overrides/FooterWrapper.astro",
+          Header: './src/components/overrides/Header.astro',
+          Sidebar: './src/components/overrides/Sidebar.astro',
+          Pagination: './src/components/overrides/Pagination.astro',
+          Footer: './src/components/overrides/FooterWrapper.astro',
       },
       customCss: ["./src/tailwind.css"],
       head: [
@@ -59,4 +60,6 @@ export default defineConfig({
     tailwind(),
     react(),
   ],
+  output: 'server',
+  adapter: netlify(),
 });
