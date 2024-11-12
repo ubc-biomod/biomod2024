521e6bd72ac1ff49ec59f4598c6ac7d83602cf08
Minh Au <auduongminh2004@gmail.com>
2024-11-12 15:32:56 -0800

    add math render

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,51 +8,66 @@ import react from "@astrojs/react";
 
 import netlify from '@astrojs/netlify';
 
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-      title: 'UBC BIOMOD',
+  integrations: [
+    starlight({
+      title: "UBC BIOMOD",
       social: {
-          github: 'https://github.com/withastro/starlight',
+        github: "https://github.com/withastro/starlight",
       },
       sidebar: [
-          {
-              label: 'Home',
-              link: '/',
-              badge: 'Hidden'
+        {
+          label: "Home",
+          link: "/",
+          badge: "Hidden",
+        },
+        {
+          label: "Ideas",
+          autogenerate: { directory: "ideas" },
+        },
+        {
+          label: "ELSI",
+          autogenerate: { directory: "elsi" },
+        },
+        {
+          label: "Lab Notebook",
+          autogenerate: { directory: "lab-notebook" },
+          badge: {
+            text: "Call to Action",
           },
-          {
-              label: 'Ideas',
-              autogenerate: { directory: 'ideas' },
-          },
-          {
-              label: 'ELSI',
-              autogenerate: { directory: 'elsi' },
-          },
-          {
-              label: 'Lab Notebook',
-              autogenerate: { directory: 'lab-notebook' },
-              badge: {
-                  text: 'Call to Action',
-              }
-          },
-          {
-              label: 'Sponsors',
-              link: '/sponsors',
-              badge: 'Hidden' 
-          },
+        },
+        {
+          label: "Sponsors",
+          link: "/sponsors",
+          badge: "Hidden",
+        },
       ],
       components: {
-          Header: './src/components/overrides/Header.astro',
-          Sidebar: './src/components/overrides/Sidebar.astro',
-          Pagination: './src/components/overrides/Pagination.astro',
-          Footer: './src/components/overrides/FooterWrapper.astro',
+        Header: "./src/components/overrides/Header.astro",
+        Sidebar: "./src/components/overrides/Sidebar.astro",
+        Pagination: "./src/components/overrides/Pagination.astro",
+        Footer: "./src/components/overrides/FooterWrapper.astro",
       },
-      customCss: [
-          './src/tailwind.css',
-      ]
-      }), tailwind(), react()],
-
-  output: 'server',
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind(),
+    react(),
+  ],
+  vite: {
+    ssr: {
+      noExternal: [
+        "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css",
+      ],
+    },
+  },
+  output: "server",
   adapter: netlify(),
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
