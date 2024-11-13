4f4dcb6a4831880e0075522a7f9bbbe01fe91c4e
Minh Au <auduongminh2004@gmail.com>
2024-11-13 14:20:05 -0800

    change linting configs

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-  "*.{js,jsx,ts,tsx}": ["prettier --write"],
+  "*.{js,jsx,ts,tsx,mdx}": ["prettier --write"],
   "**/*.ts?(x)": () => "npm run type-check",
   "*.json": ["prettier --write"],
 };
