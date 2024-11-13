dcf58db43133707de9ae31686bcb0b853d01563e
Minh Au <auduongminh2004@gmail.com>
2024-11-13 13:49:26 -0800

    properly fix tailwindcss

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -9,5 +11,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [starlightPlugin()],
 };
